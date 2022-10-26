ce9b9ebecd3e861d41577fefec9d4c23d5c91892
fanghsiu <fanghsiu@outlook.com>
2022-10-26 18:01:04 +0800

    Initial commit

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
-@REM 如果没有消息后缀，默认提交信息为 `周日 2022/09/25  8:26:18.79`
+@REM 如果没有消息后缀，默认提交信息为 Initial commit
 @echo off
 set info=%1
-if "%info%"=="" ( set info='Initial commit' )
+if "%info%"=="" ( set info=Initial commit )
 git add -A
 git commit -m "%info%"
 git push
