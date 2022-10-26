96a9f79102f8060c7b4161dfd60f4f822be7ed18
fanghsiu <fanghsiu@outlook.com>
2022-10-26 18:08:47 +0800

    delete img

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
-@REM 如果没有消息后缀，默认提交信息为 Initial commit
+@REM If there is no message suffix, the default commit message is "Initial commit"
 @echo off
-set info=%1
+set info=%~1
 if "%info%"=="" ( set info=Initial commit )
 git add -A
 git commit -m "%info%"
